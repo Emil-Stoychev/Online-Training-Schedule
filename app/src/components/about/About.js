c657f4f7d36b69a45681f6ed72468f2920baae3a
Емил Стойчев <estallinone@gmail.com>
2022-12-30 00:12:18 +0200

    Add design to about page

--- app/src/components/about/About.js
+++ app/src/components/about/About.js
@@ -2,8 +2,30 @@ import './about.css'
 
 export const AboutComponent = () => {
     return (
-        <>
-            <h1>About page</h1>        
-        </>
+        <div className='about-cont'>
+
+            <h1>Our Contacts</h1>
+
+            <div className='about-contacts'>
+
+                <div>
+                    <i className="fa-brands fa-facebook"></i>
+                </div>
+
+                <div>
+                    <i className="fa-brands fa-square-instagram"></i>
+                </div>
+
+                <div>
+                    <i className="fa-brands fa-github"></i>
+                </div>
+
+                <div>
+                    <i className="fa-brands fa-linkedin"></i>
+                </div>
+
+            </div>
+
+        </div>
     )
 }
