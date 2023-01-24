350bd9b30c0b815d93b3837ae41bd155eb6f25fd
Емил Стойчев <estallinone@gmail.com>
2023-01-24 22:13:05 +0200

    Start with add program comp

--- app/src/components/training/add-program/Add-program.js
+++ app/src/components/training/add-program/Add-program.js
@@ -1,57 +1,105 @@
+import { useEffect, useState } from 'react'
+import { v4 as uuid } from 'uuid';
 import './add-program.css'
 
 export const AddProgramComponent = () => {
+    const [container, setContainer] = useState([])
+    const [category, setCategory] = useState({
+        option: true,
+        value: ''
+    })
+
+    const handleBtns = (option) => {
+        let id = uuid()
+        let obj = { [option]: '', id, option }
+
+        setContainer(state => [...state, obj])
+    }
+
+    const removeFieldFromCnt = (id) => {
+        setContainer(state => state.filter(x => x.id != id))
+    }
+
+    const categoryValue = (e) => {
+        setCategory(state => ({
+            ...state,
+            value: e.target.value
+        }))
+    }
+
+    const onCreateBtnHandler = () => {
+        console.log(container);
+        console.log(category);
+    }
+
     return (
         <section className='add-program'>
             <h1>Add program</h1>
 
             <div className='add-program-buttons'>
-                <button>Title + </button>
-                <button>Description + </button>
-                <button>Rest Time +</button>
-                <button>Exercise Time +</button>
-                <button>Image + </button>
+                <button onClick={() => handleBtns('title')}>Title + </button>
+                <button onClick={() => handleBtns('description')}>Description + </button>
+                <button onClick={() => handleBtns('restTime')}>Rest Time +</button>
+                <button onClick={() => handleBtns('exerciseTime')}>Exercise Time +</button>
+                <button onClick={() => handleBtns('image')}>Image + </button>
             </div>
 
             <div className='choose-category'>
-                <button>Choose category</button>
-                <input type='text' placeholder='Add category name' />
+                <button onClick={() => setCategory(x => ({ value: '', option: !x.option }))}>Choose category</button>
 
-                {/* <select>
-                    <option>Body</option>
-                    <option>Cardio</option>
-                    <option>Outside</option>
-                </select> */}
+                {category.option
+                    ?
+                    <input value={category.value} onChange={(e) => categoryValue(e)} type='text' placeholder='Add category name' />
+                    :
+                    <select onChange={(e) => categoryValue(e)}>
+                        <option>Body</option>
+                        <option>Cardio</option>
+                        <option>Outside</option>
+                    </select>
+                }
             </div>
 
             <div className='add-option'>
-                <div>
-                    <input type='text' placeholder='Exercise name' />
-                    <h2>&#x2715;</h2>
-                </div>
-
-                <div>
-                    <input type='file' />
-                    <h2>&#x2715;</h2>
-                </div>
-
-                <div>
-                    <textarea placeholder='More info here'></textarea>
-                    <h2>&#x2715;</h2>
-                </div>
-
-                <div>
-                    <input type='number' id='rest-time' placeholder='Rest time (optional)' />
-                    <h2>&#x2715;</h2>
-                </div>
-
-                <div>
-                    <input type='number' id='exercise-time' placeholder='Exercise time (optional)' />
-                    <h2>&#x2715;</h2>
-                </div>
+                {container.length > 0 &&
+                    container.map((x) =>
+                        x.option == 'title'
+                            ?
+                            <div key={x.id}>
+                                <input type='text' placeholder='Exercise name' />
+                                <h2 onClick={() => removeFieldFromCnt(x.id)}>&#x2715;</h2>
+                            </div>
+                            :
+                            x.option == 'description'
+                                ?
+                                <div key={x.id}>
+                                    <textarea placeholder='More info here'></textarea>
+                                    <h2 onClick={() => removeFieldFromCnt(x.id)}>&#x2715;</h2>
+                                </div>
+                                :
+                                x.option == 'restTime'
+                                    ?
+                                    <div key={x.id}>
+                                        <input type='number' id='rest-time' placeholder='Rest time (optional)' />
+                                        <h2 onClick={() => removeFieldFromCnt(x.id)}>&#x2715;</h2>
+                                    </div>
+                                    :
+                                    x.option == 'image'
+                                        ?
+                                        <div key={x.id}>
+                                            <input type='file' />
+                                            <h2 onClick={() => removeFieldFromCnt(x.id)}>&#x2715;</h2>
+                                        </div>
+                                        :
+                                        x.option == 'exerciseTime' &&
+                                        <div key={x.id}>
+                                            <input type='number' id='exercise-time' placeholder='Exercise time (optional)' />
+                                            <h2 onClick={() => removeFieldFromCnt(x.id)}>&#x2715;</h2>
+                                        </div>
+                    )
+                }
             </div>
 
-            <button className='last-btn'>Create</button>
+            <button className='last-btn' onClick={() => onCreateBtnHandler()}>Create</button>
 
         </section>
     )
