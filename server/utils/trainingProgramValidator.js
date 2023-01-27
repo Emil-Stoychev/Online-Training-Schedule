d7916ec434ab5634f7c4ff2c1cfebdcdba72459a
Емил Стойчев <estallinone@gmail.com>
2023-01-27 22:12:39 +0200

    Update trainingProgramValidator.js

--- server/utils/trainingProgramValidator.js
+++ server/utils/trainingProgramValidator.js
@@ -1,13 +1,12 @@
 const { TrainingCategory } = require('../Models/TrainingCategory')
 const { TrainingImage } = require('../Models/TrainingImage')
+const { TrainingCnt } = require('../Models/TrainingCnt')
 const sharp = require("sharp");
 
-const trainingProgramValidator = async (container, categ, userId) => {
-    console.log('Start validator....=====================================================');
+const trainingProgramValidator = async (container, categoryId, userId, mainTitle) => {
 
     container = await Promise.all(container.map(async (x) => {
         let curr = Object.values(x)[0]
-        let parentId = Object.values(x)[0]?.id
 
         if (curr?.option == 'image') {
             let ids = []
@@ -33,22 +32,39 @@ const trainingProgramValidator = async (container, categ, userId) => {
                 ids.push(id)
             }))
 
-            return {
-                [parentId]: {
-                    image: [...ids],
-                    id: curr?.id,
-                    option: 'image'
-                }
-            }
+            return await TrainingCnt.create({
+                author: userId,
+                image: [...ids],
+                option: 'image'
+            })
+                .then(res => {
+                    return res?._id
+                })
         }
-
-        return x
+        return await TrainingCnt.create({
+            author: userId,
+            ...Object.values(x)[0]
+        })
+            .then(res => {
+                return res?._id
+            })
     }))
 
     // if (!curr?.value || curr?.value.length < 3 || curr?.value.trim() === '') {
     //     return { message: `${curr?.option} must be at least 3 characters!` }
     // }
 
+    let data = {
+        container: [...container],
+        category: categoryId,
+        author: userId,
+        mainTitle
+    }
+
+    return data
+}
+
+const checkAndMakeCategory = async (categ, userId, option) => {
     if (categ.value == '' || categ.value.trim() == '') {
         return { message: 'Category is not valid!' }
     }
@@ -61,24 +77,19 @@ const trainingProgramValidator = async (container, categ, userId) => {
             category: categ.value
         }
 
-        await TrainingCategory.create(categoryData)
-            .then(res => {
-                return foundCategory.push(res)
-            })
+        return await TrainingCategory.create(categoryData)
+    } else {
+        if (option) {
+            return { message: 'This category is already exist!' }
+        } else {
+            return foundCategory
+        }
     }
-
-    let data = {
-        container: [...container],
-        category: foundCategory[0]?._id,
-        author: userId
-    }
-
-    console.log('FINAL ====================!=================');
-    return data
 }
 
 // container: { type: Array },
 
 module.exports = {
-    trainingProgramValidator
+    trainingProgramValidator,
+    checkAndMakeCategory
 }
